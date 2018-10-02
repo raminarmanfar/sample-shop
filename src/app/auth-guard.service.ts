e7f186dd266e7e672c6e8d84b4439bdedfa43f66
raminarmanfar <ramin.armanfar@msg.group>
2018-10-02 11:32:55 +0200

    some syntaxes fixed

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -6,13 +6,13 @@ import 'rxjs/add/operator/map';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate{
+export class AuthGuardService implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
   canActivate (route, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
-      if (user) return true;
+      if (user) { return true; }
 
       this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
       return false;
